ddd2e463e84617c6f41607bfab52a1f4eb34a632
Gigi M <mmgamao1@gmail.com>
2019-09-26 21:24:02 +0000

    scrape product names

--- my_furniture_cli/lib/my_furniture_cli.rb
+++
@@ -1,7 +0,0 @@
-require_relative "./my_furniture_cli/version"
-require_relative "./my_furniture_cli/cli"
-
-module MyFurnitureCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- my_furniture_cli/lib/my_furniture_cli/cli.rb
+++
@@ -1,6 +0,0 @@
-class MyFurnitureCli::CLI  
-  def call
-    puts 'Welcome to My Furniture CLI!'
-    binding.pry
-  end
-end
